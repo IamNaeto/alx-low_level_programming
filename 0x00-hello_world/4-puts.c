75e506ef8a49ac27aeab66f47269e317526f546c
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2022-09-08 10:12:36 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,37 @@
-#!/bin/bash
 #include <stdio.h>
 
-int main(void) {
-	char string[70] = "\"Programming is like building a multilingual puzzle\""
 
-	puts(string);
 
-	put("\"Programming is like building a multilingual puzzle\"");
-	return (0);
-}
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Author: Sammykingx
+ *
+ *     *
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
+
+int main(void)
+
+{
+
+		char string[70] = "\"Programming is like building a multilingual puzzle\"";
+
+			
+
+			puts(string);
+
+
+
+				/* Tried printing it directly */
+
+
+
+				puts("\"Programming is like building a multilingual puzzle\"");
+
+					return (0);
