cf8264f9408e80d01a7fee234972ede5356b59c9
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2022-09-08 15:54:31 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * - Dora Korpar, 2015-10-19",
  *   followed by a new line, to the standard error.
  *   Return: Always 0 (Success)
- *   */
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
