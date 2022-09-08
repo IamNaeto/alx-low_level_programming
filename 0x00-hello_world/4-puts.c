895dbde00a41f023684beaf1fdfa9503ee1652ea
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2022-09-08 11:04:15 -0700

    Hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * 
@@ -7,7 +6,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
