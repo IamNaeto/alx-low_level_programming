9a8d93eb2cbe3aa57598cbf2dff57c7aa15f8de0
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2022-09-09 10:48:46 -0700

    advanced tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - program compilation begins from main
+ * Description: Program to print all possible combinations of numbers with no two numbers same in paring
+ * author: IamNaeto
+ * Return: 0 means success
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48 ; a <= 57 ; a++)
+	{
+		for (b = 49 ; b <= 57 ; b++)
+		{
+			if (a !=b && b > a)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 100 ; i++)
+	{
+		if (i < j)
+		{
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
+			putchar(' ');
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
